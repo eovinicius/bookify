9a0a9bed09232c5d7fb6b56126abdb68d67f4dcb
Vinicius Oliveira <eovinicius10@gmail.com>
2025-03-06 23:26:20 -0300

    feat: Add domain events to Entity base class

--- src/Bookify.Domain/Entities/Abstractions/Entity.cs
+++ src/Bookify.Domain/Entities/Abstractions/Entity.cs
@@ -3,9 +3,26 @@ namespace Bookify.Domain.Entities.Abstractions;
 public abstract class Entity
 {
     public Guid Id { get; init; }
+    public readonly List<IDomainEvent> DomainEvents;
 
     protected Entity(Guid id)
     {
         Id = id;
+        DomainEvents = [];
+    }
+
+    protected void Raise(IDomainEvent domainEvent)
+    {
+        DomainEvents.Add(domainEvent);
+    }
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return DomainEvents.AsReadOnly();
+    }
+
+    public void Clear()
+    {
+        DomainEvents.Clear();
     }
 }
