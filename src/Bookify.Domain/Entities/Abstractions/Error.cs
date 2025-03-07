0a578613b03436488105e19bdd8a01d23573d0e3
Vinicius Oliveira <eovinicius10@gmail.com>
2025-03-07 14:46:41 -0300

    feat: Add Error entity to represent domain errors

---
+++ src/Bookify.Domain/Entities/Abstractions/Error.cs
@@ -0,0 +1,7 @@
+namespace Bookify.Domain.Entities.Abstractions;
+
+public record Error(string Code, string Name)
+{
+    public static Error None = new Error(string.Empty, string.Empty);
+    public static Error NullValue = new Error("Error.NullValue", "Null value was provided");
+}
