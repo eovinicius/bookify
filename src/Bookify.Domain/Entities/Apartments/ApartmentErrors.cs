44f2cf49ead2272e1df6a3e62dde2cbbcf0e383c
Vinicius Oliveira <eovinicius10@gmail.com>
2025-03-07 15:14:09 -0300

    feat: Add ApartmentErrors class for apartment-related domain errors

---
+++ src/Bookify.Domain/Entities/Apartments/ApartmentErrors.cs
@@ -0,0 +1,10 @@
+﻿using Bookify.Domain.Entities.Abstractions;
+
+namespace Bookify.Domain.Entities.Apartments;
+
+public static class ApartmentErrors
+{
+    public static Error NotFound = new(
+        "Apartment.NotFound",
+        "The apartment with the specified identifier was not found");
+}
