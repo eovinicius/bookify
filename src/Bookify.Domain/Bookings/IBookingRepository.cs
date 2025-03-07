b2a957e3eb02703afb8061afea9deb14fc0f9184
Vinicius Oliveira <eovinicius10@gmail.com>
2025-03-07 18:46:47 -0300

    feat: Add IsOverlappingAsync method to IBookingRepository

--- src/Bookify.Domain/Bookings/IBookingRepository.cs
+++ src/Bookify.Domain/Bookings/IBookingRepository.cs
@@ -1,8 +1,16 @@
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings.ValueObjects;
+
 namespace Bookify.Domain.Bookings;
 
 public interface IBookingRepository
 {
     Task<Booking?> GetById(Guid id, CancellationToken cancellationToken = default);
+
+    Task<bool> IsOverlappingAsync(
+        Apartment apartment,
+        DateRange duration,
+        CancellationToken cancellationToken = default);
 
     void Add(Booking booking);
 }
