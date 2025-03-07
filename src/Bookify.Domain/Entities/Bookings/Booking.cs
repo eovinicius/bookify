eadfdb57cddd74bc96644fbb4acf0d4de73ea9a8
Vinicius Oliveira <eovinicius10@gmail.com>
2025-03-07 11:59:09 -0300

    feat: Add Booking entity to manage reservations

---
+++ src/Bookify.Domain/Entities/Bookings/Booking.cs
@@ -0,0 +1,69 @@
+using Bookify.Domain.Entities.Abstractions;
+using Bookify.Domain.Entities.Bookings.ValueObjects;
+using Bookify.Domain.Entities.Shared;
+
+namespace Bookify.Domain.Entities.Bookings;
+
+public sealed class Booking : Entity
+{
+    public Guid ApartmentId { get; private set; }
+    public Guid UserId { get; private set; }
+    public DateRange Duration { get; private set; }
+    public Money PriceForPeriod { get; private set; }
+    public Money CleaningFee { get; private set; }
+    public Money AmenitiesUpCharge { get; private set; }
+    public Money TotalPrice { get; private set; }
+    public BookingStatus Status { get; private set; }
+    public DateTime CreatedOnUtc { get; private set; }
+    public DateTime? ConfirmedOnUtc { get; private set; }
+    public DateTime? RejectedOnUtc { get; private set; }
+    public DateTime? CompletedOnUtc { get; private set; }
+    public DateTime? CancelledOnUtc { get; private set; }
+
+    private Booking(
+        Guid id,
+        Guid apartmentId,
+        Guid userId,
+        DateRange duration,
+        Money priceForPeriod,
+        Money cleaningFee,
+        Money amenitiesUpCharge,
+        Money totalPrice,
+        BookingStatus status,
+        DateTime createdOnUtc)
+            : base(id)
+    {
+        ApartmentId = apartmentId;
+        UserId = userId;
+        Duration = duration;
+        PriceForPeriod = priceForPeriod;
+        CleaningFee = cleaningFee;
+        AmenitiesUpCharge = amenitiesUpCharge;
+        TotalPrice = totalPrice;
+        Status = status;
+        CreatedOnUtc = createdOnUtc;
+    }
+
+    public static Booking Reverse(
+        Guid apartmentId,
+        Guid userId,
+        DateRange duration,
+        DateTime utcNow,
+        PricingDetails pricingDetails)
+    {
+        var booking = new Booking(
+            Guid.NewGuid(),
+            apartmentId,
+            userId,
+            duration,
+            pricingDetails.PriceForPeriod,
+            pricingDetails.CleaningFee,
+            pricingDetails.AmenitiesUpcharge,
+            pricingDetails.TotalPrice,
+            BookingStatus.Reserved,
+            utcNow);
+
+        booking.Raise(new BookingReservedEvent(booking.Id));
+        return booking;
+    }
+}
---
+++ src/Bookify.Domain/Entities/Bookings/BookingStatus.cs
@@ -0,0 +1,10 @@
+namespace Bookify.Domain.Entities.Bookings;
+
+public enum BookingStatus
+{
+    Reserved = 1,
+    Confirmed = 2,
+    Rejected = 3,
+    Cancelled = 5,
+    Completed = 4
+}
