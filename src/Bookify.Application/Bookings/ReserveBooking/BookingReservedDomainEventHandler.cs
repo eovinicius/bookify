297ffe8f743598eac703f459a6f358900a82ffe2
Vinicius Oliveira <eovinicius10@gmail.com>
2025-03-07 19:38:40 -0300

    feat: add handler for booking reserved event

---
+++ src/Bookify.Application/Bookings/ReserveBooking/BookingReservedDomainEventHandler.cs
@@ -0,0 +1,38 @@
+using Bookify.Application.Abstractions.Email;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Bookings.Events;
+using Bookify.Domain.Users;
+using MediatR;
+
+namespace Bookify.Application.Bookings.ReserveBooking;
+
+internal sealed class BookingReservedDomainEventHandler : INotificationHandler<BookingReservedDomainEvent>
+{
+    private readonly IBookingRepository _bookingRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly IEmailService _emailService;
+
+    public BookingReservedDomainEventHandler(
+        IBookingRepository bookingRepository,
+        IUserRepository userRepository,
+        IEmailService emailService)
+    {
+        _bookingRepository = bookingRepository;
+        _userRepository = userRepository;
+        _emailService = emailService;
+    }
+
+    public async Task Handle(BookingReservedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var booking = await _bookingRepository.GetById(notification.BookingId, cancellationToken);
+        if (booking is null) return;
+
+        var user = await _userRepository.GetById(notification.BookingId, cancellationToken);
+        if (user is null) return;
+
+        await _emailService.Send(
+            user.Email,
+            "Booking reserved!",
+            "You have 10 minutes to confim this booking");
+    }
+}
