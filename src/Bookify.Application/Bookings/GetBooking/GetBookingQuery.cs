6c36bb5b61db7e88fbbf2aedfbeb7ceaef6d94b3
Vinicius Oliveira <eovinicius10@gmail.com>
2025-03-08 10:52:59 -0300

    feat: Add GetBooking query and handler

---
+++ src/Bookify.Application/Bookings/GetBooking/GetBookingQuery.cs
@@ -0,0 +1,8 @@
+using Bookify.Application.Abstractions.Messaging;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+public record GetBookingQuery(Guid BookingId) : IQuery<BookingResponse>
+{
+
+}
