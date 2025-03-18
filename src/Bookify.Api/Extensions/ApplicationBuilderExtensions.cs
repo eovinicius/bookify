e51c08a7f4777d949c5ecc0e550b9e3e7164b22e
Vinicius Oliveira <eovinicius10@gmail.com>
2025-03-18 12:06:22 -0300

    feat: Apply database migrations on startup

---
+++ src/Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+using Bookify.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Api.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using var scoop = app.ApplicationServices.CreateScope();
+
+        using var dbContext = scoop.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        dbContext.Database.Migrate();
+    }
+}
